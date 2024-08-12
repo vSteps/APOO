c26c19496fb8ad525878ed3356129d4d01ca1d01
Victor Passos <131535916+vSteps@users.noreply.github.com>
2024-08-12 12:35:00 +0000

    Atividade de Sistema de Uber quase finalizada

--- mysite/SistemaUber/business.py
+++ mysite/SistemaUber/business.py
@@ -9,20 +9,20 @@ class CalculaCorrida:
 
 class CalculaCorridaUberX(CalculaCorrida):
     def get_valor(self):
-        return 0.9 * super().get_valor()
+        return super().get_valor()
 
 class CalculaCorridaUberMoto(CalculaCorrida):
     def get_valor(self):
-        return 1.1 * super().get_valor()
+        return 0.9 * super().get_valor()
 
 class CalculaCorridaUberBlack(CalculaCorrida):
     def get_valor(self):
-        return super().get_valor()
+        return 1.1 * super().get_valor()
 
 class CalculaCorridaUberPool(CalculaCorrida):
-    def __init__(self, km, preco_km=5, passageiros=1):
+    def __init__(self, km, preco_km=5, numPassageiros=1):
         super().__init__(km, preco_km)
-        self.passageiros = passageiros
+        self.numPassageiros = numPassageiros
 
     def get_valor(self):
-        return super().get_valor() / self.passageiros
+        return super().get_valor() / self.numPassageiros
